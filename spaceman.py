7d8d495e55abe152df44e1dceac04f37d812c60f
Ivan Yerkinov <ivan@Ivans-MacBook-Pro.local>
2019-09-05 13:37:56 -0700

    Begin Spaceman Project

---
+++ spaceman.py
@@ -0,0 +1,18 @@
+
+class gameEn:
+
+    def __init__(self):
+        # Initialise
+        self.word
+
+    def mainGameLoop(self):
+        # Main loop goes here
+
+    def guessLetter(letter):
+        # Guess letter
+
+    def printWin():
+        # Print winning message
+
+    def printLose():
+        # Print losing message
