01e48bd377b15c747cf280390c4ae8d2191bbbba
Ivan Yerkinov <ivan@Ivans-MacBook-Pro.local>
2019-09-08 11:41:39 -0700

    Finished base spaceman code

--- spaceman.py
+++ spaceman.py
@@ -1,18 +1,74 @@
-
 class gameEn:
 
-    def __init__(self):
+    def __init__(self, word):
         # Initialise
-        self.word
+        self.word = list(word)
+        self.shownWord = list(word)
+        self.tIncGuess = 7
+        self.nIncGuess = 0
+        self.gLetter = []
+        self.incLetter = []
+
+        for l in range(0, len(self.shownWord)):
+            self.shownWord[l] = '_'
 
     def mainGameLoop(self):
         # Main loop goes here
+        gameLoop = True
+        while gameLoop:
+            self.printHUD()
+            while self.guessLetter(input("Please guess a letter\n")):
+                print()
+            if self.word == self.shownWord:
+                gameLoop = False
+                self.printWin()
+            if self.tIncGuess == self.nIncGuess:
+                gameLoop = False
+                self.printLose()
 
-    def guessLetter(letter):
+    def guessLetter(self, letter):
         # Guess letter
+        if len(letter) > 1:
+            return True
 
-    def printWin():
+        if letter.isalpha():
+
+            if letter in self.gLetter:
+                return True
+            else:
+                self.gLetter.append(letter)
+                self.proccesLetter(letter)
+                return False
+
+        else:
+            return True
+
+    def proccesLetter(self, letter):
+        if letter in self.word:
+            for i in range(0, len(self.word)):
+                if self.word[i] == letter:
+                    self.shownWord[i] = letter
+        else:
+            self.incLetter.append(letter)
+            self.nIncGuess += 1
+
+    def printWin(self):
         # Print winning message
+        print("\033[1;33;40m" + "CONGRAGULATION! YOU ARE WINNER!" + "\033[1;37;40m")
 
-    def printLose():
-        # Print losing message
+    def printLose(self):
+        print("\033[1;31;40m" + "YOU ARE THE LOSE" + "\033[1;37;40m")
+
+    def printHUD(self):
+        print('\n' * 100)
+        print("SPACEMAN")
+        temp = self.tIncGuess - self.nIncGuess
+        print("Guesses left: " + str(temp) + "\n")
+        print(str(self.shownWord) + "\n")
+        print("Incorrect Guesses: " + str(self.incLetter))
+        print("Total Guesses: " + str(self.gLetter))
+
+
+game = gameEn("Test")
+
+game.mainGameLoop()
